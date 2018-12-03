8aa0d052bd16080aea6370e63711939623750137
Kyle Blocher <kdblocher@gmail.com>
2018-12-03 01:46:36 -0600

    both day 1 and first day 2

---
+++ AdventOfCode/Day1.fs
@@ -0,0 +1,60 @@
+﻿module Day1
+open System.Collections.Generic
+open System.IO
+open Xunit
+
+let parseInput (input : string) =
+  input.Split '\n' |> Seq.map int
+
+let calibrate =
+  parseInput >> Seq.sum
+
+let calibrateTestData : obj [] [] = [|
+  [| 3; "+1\n+1\n+1" |]
+  [| 0; "+1\n+1\n-2" |]
+  [| -6; "-1\n-2\n-3" |]
+|]
+
+[<Theory>]
+[<MemberData("calibrateTestData")>]
+let ``Calibrate Tests`` expected input =
+  let actual = calibrate input
+  Assert.Equal (expected, actual)
+
+[<Fact>]
+let ``Calibrate Actual`` () =
+  let input = File.ReadAllText "Day1input.txt"
+  let actual = calibrate input
+  Assert.Equal (520, actual)
+
+let infiniteInput input =
+  let frequencies = parseInput input
+  let rec f () = seq { yield! frequencies; yield! f () }
+  f ()
+
+let findRepeat (input : string) =
+  let seen = HashSet<int> ()
+  let items = (infiniteInput input).GetEnumerator()
+  let mutable cur = 0
+  while seen.Add cur && items.MoveNext() do
+    cur <- cur + items.Current
+  cur
+
+let findRepeatTestData : obj [] [] = [|
+  [| 0; "+1\n-1" |]
+  [| 10; "+3\n+3\n+4\n-2\n-4" |]
+  [| 5; "-6\n+3\n+8\n+5\n-6" |]
+  [| 14; "+7\n+7\n-2\n-7\n-4" |]
+|]
+
+[<Theory>]
+[<MemberData("findRepeatTestData")>]
+let ``Find Repeat Tests`` expected input =
+  let actual = findRepeat input
+  Assert.Equal (expected, actual)
+
+[<Fact>]
+let ``Find Repeat Actual`` () =
+  let input = File.ReadAllText "Day1input.txt"
+  let actual = findRepeat input
+  Assert.Equal (394, actual)
---
+++ AdventOfCode/Day2.fs
@@ -0,0 +1,52 @@
+﻿module Day2
+open System.Collections.Generic
+open System.IO
+open Xunit
+
+let findRepeats (input : string) =
+  let counts = Array.replicate 26 0
+  let f (c : char) =
+    counts.[int c - int 'a'] <- counts.[int c - int 'a'] + 1
+  input |> Seq.iter f
+  counts |> Seq.exists ((=) 2),
+  counts |> Seq.exists ((=) 3)
+
+let bool2int b = if b then 1 else 0
+let checksum ls =
+  ls
+  |> Seq.map (fun (a, b) -> (bool2int a, bool2int b))
+  |> Seq.reduce (fun (a, b) (c, d) -> a + c, b + d)
+  |> (fun (a, b) -> a * b)
+
+
+
+let checksumTestData : obj [] [] = [|
+  [| false; false; "abcdef" |]
+  [| true ; true ; "bababc" |]
+  [| true ; false; "abbcde" |]
+  [| false; true ; "abcccd" |]
+  [| true ; false; "aabcdd" |]
+  [| true ; false; "abcdee" |]
+  [| false; true ; "ababab" |]
+|]
+
+[<Theory>]
+[<MemberData("checksumTestData")>]
+let ``Checksum Tests - Find Repeats`` hasTwo hasThree input =
+  let actual = findRepeats input
+  Assert.Equal ((hasTwo, hasThree), actual)
+
+[<Fact>]
+let ``Checksum Tests - Checksum`` () =
+  let actual =
+    checksumTestData
+    |> Seq.map (fun x -> x.[2].ToString())
+    |> Seq.map findRepeats
+    |> checksum
+  Assert.Equal (12, actual)
+
+[<Fact>]
+let ``Checksum Actual`` () =
+  let input = File.ReadAllLines "Day2input.txt"
+  let actual = input |> Seq.map findRepeats |> checksum
+  Assert.Equal (5704, actual)
