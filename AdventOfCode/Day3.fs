f631f038f7b8dccfa5e4027dcd6f46ec836bf63f
Kyle Blocher <kyle.blocher@blocherconsulting.com>
2018-12-05 12:16:27 -0600

    finally, area calculation appears to work

--- AdventOfCode/Day3.fs
+++ AdventOfCode/Day3.fs
@@ -4,6 +4,7 @@ open System.IO
 open System.Text
 open Xunit
 open System.Text.RegularExpressions
+open System
 
 type Rectangle = {
   Left : int
@@ -17,7 +18,7 @@ type Rectangle = {
 
 type Claim = {
   Number : int
-  Rectangle : Rectangle
+  Envelope : Rectangle
 }
 
 let parseRegex = Regex ("#(\d+) @ (\d+),(\d+): (\d+)x(\d+)", RegexOptions.Singleline)
@@ -26,7 +27,7 @@ let parseInputLine line =
   let getValue index = groups.[index + 1].Value |> int
   {
     Number = getValue 0
-    Rectangle = {
+    Envelope = {
       Left   = getValue 1
       Top    = getValue 2
       Width  = getValue 3
@@ -40,9 +41,9 @@ let overlapInputData = [|
   "#3 @ 5,5: 2x2"
 |]
 let overlapTestData : obj [] [] = [|
-  [| { Number = 1; Rectangle = { Left = 1; Top = 3; Width = 4; Height = 4 } }; overlapInputData.[0] |]
-  [| { Number = 2; Rectangle = { Left = 3; Top = 1; Width = 4; Height = 4 } }; overlapInputData.[1] |]
-  [| { Number = 3; Rectangle = { Left = 5; Top = 5; Width = 2; Height = 2 } }; overlapInputData.[2] |]
+  [| { Number = 1; Envelope = { Left = 1; Top = 3; Width = 4; Height = 4 } }; overlapInputData.[0] |]
+  [| { Number = 2; Envelope = { Left = 3; Top = 1; Width = 4; Height = 4 } }; overlapInputData.[1] |]
+  [| { Number = 3; Envelope = { Left = 5; Top = 5; Width = 2; Height = 2 } }; overlapInputData.[2] |]
 |]
 
 [<Theory>]
@@ -51,72 +52,79 @@ let ``Overlap Tests - Parse`` claim input =
   let actual = parseInputLine input
   Assert.Equal (claim, actual)
 
-type Intersection = {
-  Rectangle : Rectangle
-  RemainingArea : int
+type Interval = {
+  Start : int
+  End : int
 }
 
-let findOverlap (r1 : Rectangle, r2 : Rectangle) =
-  let left   = max r1.Left r2.Left
-  let right  = min r1.Right r2.Right
-  let top    = max r1.Top r2.Top
-  let bottom = min r1.Bottom r2.Bottom
-  if (left < right && top < bottom) then
-    let intersection = {
-      Left = left
-      Top = top
-      Width = right - left
-      Height = bottom - top
-    }
-    Some {
-      Rectangle = intersection
-      RemainingArea = r1.Area + r2.Area - (2 * intersection.Area)
-    }
-    else None
- 
-let inline permute x = (Helpers.permute >> Helpers.removeDupes) x
-let getRects =
-  let getRect = parseInputLine >> (fun c -> c.Rectangle)
-  permute >> Seq.map (fun (a, b) -> getRect a, getRect b)
+type Overlap = {
+  Intervals : Interval list
+} with
+  member this.TotalInterval = {
+    Start = this.Intervals |> List.map (fun l -> l.Start) |> List.min
+    End   = this.Intervals |> List.map (fun l -> l.End) |> List.max
+  }
 
-let findIntersectionOverlaps =
-  Seq.map findOverlap >> Seq.choose id
- 
-let findAllOverlaps =
-  getRects >> findIntersectionOverlaps
+let inline (><?) s1 s2 = s1.Start < s2.End || s1.End > s2.Start
+let inline (>-<) s1 s2 = if s1 ><? s2 then Some { Start = s1.Start; End = s2.End } else None
+let inline (<->) s1 s2 =
+  if s1.Start < s2.Start && s1.End < s2.End then [ { Start = s1.Start; End = s2.Start }; { Start = s2.End; End = s1.End } ]
+  else if s1.Start < s2.Start then [ { Start = s1.Start; End = s2.Start }]
+  else [ { Start = s2.End; End = s2.End }]
+let inline (<) s1 s2 = s1.End < s2.Start
+let getYInterval r = { Start = r.Top; End = r.Bottom }
+let getArea = (*) << List.sumBy (fun ys -> ys.End - ys.Start) << List.map (fun (o : Overlap) -> o.TotalInterval)
 
-let transformIntersections =
-  let f (acc, rs) i = (acc + i.RemainingArea, Seq.append rs (Seq.singleton i.Rectangle))
-  Seq.fold f (0, Seq.empty)
+let rec driver update zero i (os : Overlap list) =
+  match os with
+  | [] -> zero ()
+  | o :: os when i ><? o.TotalInterval -> { Intervals = update i o.Intervals } :: os
+  | o :: os -> o :: driver update zero i os
+
+let rec insert i =
+  let rec f i ys =
+    match ys with
+    | [] -> [i]
+    | y :: ys when i < y -> i :: y :: ys
+    | y :: ys -> y :: f i ys
+  driver f (fun () -> [{ Intervals = [i] }]) i
+
+let rec remove =
+  let rec f i ys =
+    match ys with
+    | [] -> []
+    | y :: ys when i = y -> ys
+    | y :: ys -> y :: f i ys
+  driver f (fun () -> failwith "Not supported")
+
+
+type XEventType = Start of Rectangle | End of Rectangle
+type XEvent = {
+  X : int
+  Event : XEventType
+}
+
+let sweep (rects : Rectangle seq) =
+  let rects = rects |> List.ofSeq |> List.sortBy (fun r -> r.Right)
+  let getAllXs =
+    let dup r = [
+      { X = r.Left; Event = Start r }
+      { X = r.Right; Event = End r }]
+    List.collect dup >> List.sortBy (fun x -> x.X)
+  let rec f (xs : XEvent list) (ys : Overlap list) (x0 : int) (area : int) =
+    match xs with
+    | [] -> area
+    | { X = x; Event = e } :: xs ->
+      let area0 = getArea ys (x - x0)
+      let update =
+        match e with
+        | Start r -> insert (getYInterval r)
+        | End r -> remove (getYInterval r)
+      f xs (update ys) x (area + area0)
+  f (getAllXs rects) [] 0 0
 
 [<Fact>]
 let ``Overlap Tests - Find`` () =
-  let actual = findAllOverlaps overlapInputData |> Seq.exactlyOne
-  let expected = {
-    Rectangle = {
-      Left = 3
-      Top = 3
-      Width = 2
-      Height = 2
-    }
-    RemainingArea = 24
-  }
+  let actual = overlapInputData |> (Seq.map parseInputLine >> Seq.map (fun r -> r.Envelope) >> sweep)
+  let expected = 32
   Assert.Equal (expected, actual)
-
-let walkOverlaps input =
-  let rec f acc rs =
-    let (acc0, rs0) = rs |> (permute >> findIntersectionOverlaps >> transformIntersections)
-    if Seq.isEmpty rs0
-      then acc + (rs |> Seq.sumBy (fun r -> r.Area))
-      else f (acc + acc0) rs0
-  f 0 ((findAllOverlaps >> transformIntersections >> snd) input)
-      
-[<Fact>]
-let ``Overlap Tests - Double Counting 1`` () =
-  let actual = walkOverlaps overlapInputData
-  Assert.Equal (4, actual)
-
-[<Fact>]
-let ``Overlap Tests - Double Counting 2`` () =
-  let actual = walkOverlaps (overlapInputData |> Array.append (Array.singleton "#4 @ 2,2: 2x2"))
-  Assert.Equal (7, actual)
