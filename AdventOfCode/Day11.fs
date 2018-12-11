b3425468d1283e45d22a63675c6ca63207d4988c
Kyle Blocher <kyle.blocher@blocherconsulting.com>
2018-12-11 12:29:28 -0600

    Day 11 Part 2

--- AdventOfCode/Day11.fs
+++ AdventOfCode/Day11.fs
@@ -2,51 +2,76 @@
 
 open Xunit
 
-type Cell = {
-  RackID : int
-  Power : int
-}
-
 let hundredsDigit x = (x % 1000 - x % 100) / 100
 let createCell serial x y =
   let rackID = x + 10
   let power = hundredsDigit ((rackID * y + serial) * rackID) - 5
-  { RackID = rackID; Power = power }
- 
-let cartesian xs ys = 
-  xs |> Seq.collect (fun x -> ys |> Seq.map (fun y -> x, y))
- 
-let maxPower x y minSize maxSize serial =
-  let array = Array.init x (fun i -> Array.init y (createCell serial i))
-  let powerLevelOfCell size (x, y) =
-    Array.sub array x size |> Array.sumBy (fun i -> Array.sub i y size |> Array.sumBy (fun j -> j.Power))
-  let findMax size =
-    let max = cartesian (seq { 0 .. (x - size) }) (seq { 0 .. (y - size) }) |> Seq.maxBy (powerLevelOfCell size)
-    size, max
-  seq { minSize .. maxSize } |> (Seq.map findMax >> Seq.maxBy snd)
+  power
 
-let defaultMaxPower = maxPower 300 300 3 3 >> snd
+type Result = {
+  X : int
+  Y : int
+  Size : int
+  Sum : int
+}
+
+let dp (array : _ [] []) offset (i, j) =
+  let bx = i - offset < 0
+  let by = j - offset < 0
+  let dx = if bx then 0 else array.[i - offset].[j]
+  let dy = if by then 0 else array.[i].[j - offset]
+  let dd = if bx || by then 0 else array.[i - offset].[j - offset]
+  dx + dy - dd
+
+let maxPower x y minSize maxSize serial =
+  
+  let powers =
+    Array.init x (fun i -> Array.init y (createCell serial i))
+
+  let sums =
+    let sums = Array.init x (fun i -> Array.zeroCreate<int> y)
+    let dp = dp sums 1
+    seq { 0 .. x - 1 } |> Seq.iter (fun i ->
+      seq { 0 .. y - 1 } |> Seq.iter (fun j ->
+        sums.[i].[j] <- powers.[i].[j] + dp (i, j)))
+    sums
+  
+  let findMaxResult = Seq.maxBy (fun r -> r.Sum)
+  let findMaxSquare size =
+    let dp = dp sums size
+    let allSizes =
+      seq { size - 1 .. x - 1 } |> Seq.collect (fun i ->
+        seq { size - 1 .. y - 1 } |> Seq.map (fun j -> {
+          Size = size
+          X = i - size + 1
+          Y = j - size + 1
+          Sum = sums.[i].[j] - dp (i, j)
+        }))
+    allSizes |> findMaxResult
+  seq { minSize .. maxSize } |> (Seq.map findMaxSquare >> findMaxResult)
+
+let defaultMaxPower = maxPower 300 300 3 3
 
 [<Fact>]
 let ``Max Power - Test`` () =
-  let actual = defaultMaxPower 18
+  let { X = x; Y = y } = defaultMaxPower 18
   let expected = 33, 45
-  Assert.Equal (expected, actual)
+  Assert.Equal (expected, (x, y))
 
 [<Fact>]
 let ``Max Power - Test 2`` () =
-  let actual = defaultMaxPower 42
+  let { X = x; Y = y } = defaultMaxPower 42
   let expected = 21, 61
-  Assert.Equal (expected, actual)
+  Assert.Equal (expected, (x, y))
   
 [<Fact>]
 let ``Max Power - Actual`` () =
-  let actual = defaultMaxPower 8199
+  let { X = x; Y = y } = defaultMaxPower 8199
   let expected = 235, 87
-  Assert.Equal (expected, actual)
+  Assert.Equal (expected, (x, y))
 
 [<Fact>]
 let ``Max Power Any Size - Actual`` () =
-  let (size, actual) = maxPower 300 300 1 300 8199
-  let expected = 235, 87
-  Assert.Equal (expected, actual)
+  let { X = x; Y = y; Size = size } = maxPower 300 300 1 300 8199
+  let expected = 234, 272, 18
+  Assert.Equal (expected, (x, y, size))
